1feb16024a80fd3682045e050417fd88d35f0fb6
Mridul Sikarwar <seaweed_casks_3g@icloud.com>
2026-01-12 20:14:24 +0530

    V1

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+[
+  { id: 1, title: "Post title", content: "Post content" },
+  { id: 2, title: "Another post", content: "Text..." }
+]
